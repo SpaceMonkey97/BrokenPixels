9ed7efaf6e04b30107f0839944a13872a5401782
tar98 <tarchi.giacomo2310@gmail.com>
2020-12-19 02:17:42 +0100

    Dungeon Generator - Test

---
+++ BrokenPixels/Assets/Prototypes/Dungeon Procedurale/Scripts/AddRoom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AddRoom : MonoBehaviour {
+
+	private RoomTemplates templates;
+
+	void Start(){
+
+		templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+		templates.rooms.Add(this.gameObject);
+	}
+}
